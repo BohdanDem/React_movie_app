c2fede0960ce3dcf6a7995b17223be4e77e991cd
BohdanDem <bogdandemchuk.1@gmail.com>
2023-07-27 17:17:19 +0300

    init commit

--- movie_app/src/Routing/router.js
+++ movie_app/src/Routing/router.js
@@ -1,13 +1,19 @@
 import {createBrowserRouter} from "react-router-dom";
+import {AppMainLayout} from "../AppMainLayout/AppMainLayout";
+import {AppRoutes} from "./AppRoutes";
 
 export const router = createBrowserRouter([
     {
-        element: <AppLayout/>,
-        errorElement: <h2>Ops, wrong way</h2>,
+        element: <AppMainLayout/>,
+        errorElement: <h2>The resource you requested could not be found.</h2>,
         children: [
             {
-                path: g,
+                path: AppRoutes.Movie,
                 element: <h1>Welcome</h1>,
+            },
+            {
+                path: AppRoutes.Music,
+                element: <h1>The music page isn't implemented yet</h1>,
             }
         ]
     }
